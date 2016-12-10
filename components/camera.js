6cdf6981e6f24d942ffe2f6e42e526596a918edf
Mel Booth <mel.m.booth@gmail.com>
2016-12-10 19:57:40 +1300

    snapshot - wip

--- components/camera.js
+++ components/camera.js
@@ -2,6 +2,7 @@ var yo = require('yo-yo')
 
 function accessCamera (state) {
   var video = document.getElementById("camera")
+  // video.src = window.URL.createObjectURL(stream) // may be superfluous
   navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia)
 
   if (navigator.getUserMedia) {
@@ -16,6 +17,7 @@ function accessCamera (state) {
   }
 
   function takePhoto() {
+    console.log('takePhoto !!!');
     var canvas = document.getElementById('canvas')
     var context = canvas.getContext('2d')
     var w = 300
@@ -23,25 +25,31 @@ function accessCamera (state) {
     if (w && h) {
       canvas.w = w
       canvas.h = h
+      console.log(typeof video);
+       // Uncaught TypeError: Failed to execute 'drawImage' on 'CanvasRenderingContext2D': The provided value is not of type '(HTMLImageElement or HTMLVideoElement or HTMLCanvasElement or ImageBitmap)'
+    var img = new Image()
+    img.src = canvas.toDataURL('image/png')
+    img.onload = function(){
       context.drawImage(video, 0, 0, w, h)
+    }
 
     var data = canvas.toDataURL('image/png')
-      photo.setAttribute('src', data)
+      console.log("data is", data);
+      // video.setAttribute('src', data)
     } else {
         clearphoto()
         }
   }
   return yo `
   <div>
+  <canvas id="canvas" width="300" height="300"></canvas>
   <video autoplay='true' id='camera'></video>
-  <button onclick=${accessCamera} type='submit' id='takePhoto'>oOo</button>
+  <button onclick=${takePhoto} type='submit' id='takePhoto'>oOo</button>
   </div>
   `
-// photoBtn = document.getElementById('takePhoto').addEventListener('click', function(e){
+// document.getElementById('takePhoto').addEventListener('click', function(e){
 //   e.preventDefault()
 //   takePhoto()
 // }, false)
 }
-
-
 module.exports = accessCamera
