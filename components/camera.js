07f8089a14982c1ce275d0cbea5fff9ec436f194
Mel Booth <mel.m.booth@gmail.com>
2016-12-11 15:31:11 +1300

    update accessCamera function in another attempt to make it work for mobile

--- components/camera.js
+++ components/camera.js
@@ -2,25 +2,29 @@ var yo = require('yo-yo')
 
 function accessCamera (state) {
 
-  // var handleSuccess = function(stream) {
-  //   var video = document.getElementById('video')
-  //   video.srcObject = stream
-  //
-  // }
-  // function onClick (){
-  //   var canvas = document.getElementById('snapshot')
-  //   console.log('CLICKING THE BUTTON DOES THIS');
-  //   var context = canvas.getContext('2d')
-  //   context.drawImage(video, 0, 0, canvas.width, canvas.height)
-  // }
-  //
-  // navigator.mediaDevices.getUserMedia({video: true})
-  // .then(handleSuccess)
+  var takePhoto = document.getElementById('camera')
+  var displayPhoto = document.getElementById('display')
+
+  if (takePhoto && displayPhoto) {
+    takePhoto.onchange = function (e) {
+      var files = e.target.files, file
+      if(files && files.length > 0) {
+        file = files[0]
+      else {
+        
+      }
+      }
+    }
+  }
+
+  var displayImage =
 
   return yo `
   <div>
-    <video id='video' width=300 height=300></video>
+    <div id='hideInput'><input type="file" name="file" accept="image/*" id="camera" capture="camera"></div>
     <button id='capture'>flooky</button>
+    <img src='about:blank' id='display'>
+    <button id='post'>Post</button>
     <canvas id='snapshot' width=300 height=250></canvas>
   </div>
   `
@@ -28,7 +32,23 @@ function accessCamera (state) {
 
 module.exports = accessCamera
 
-// onclick=${onClick}
+// var handleSuccess = function(stream) {
+//   var image = document.getElementById('image')
+//   video.srcObject = stream
+// }
+
+  // navigator.mediaDevices.getUserMedia({video: true})
+// .then(handleSuccess)
+
+
+// function onClick (){
+//   var canvas = document.getElementById('snapshot')
+//   var context = canvas.getContext('2d')
+//   var image = new Image()
+//   console.log('BUTTON CLICKITY');
+//   context.drawImage(image, 0, 0, canvas.width, canvas.height)
+// }
+
 
 // ##Input function - works to access camera and take photo - can't save.
-// <input type="file" name="file" accept="image/*" id="file" capture="camera">
+// <video id='video' width=300 height=300></video>
