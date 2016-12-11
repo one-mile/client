43034cc401efda54c088ea94e45f66fb2fe59731
Mel Booth <mel.m.booth@gmail.com>
2016-12-11 16:11:52 +1300

    remove comments/tidy

--- components/camera.js
+++ components/camera.js
@@ -28,31 +28,9 @@ function accessCamera (state) {
       <div id='hideInput'><input type="file" name="file" accept="image/*" id="camera" capture="camera"></div>
       <button id='capture'>flooky</button>
       <img src='about:blank' id='display'>
-      <button id='post'>Post</button>
       <canvas id='snapshot' width=300 height=250></canvas>
     </div>
     `
   }
 
 module.exports = accessCamera
-
-// var handleSuccess = function(stream) {
-//   var image = document.getElementById('image')
-//   video.srcObject = stream
-// }
-
-  // navigator.mediaDevices.getUserMedia({video: true})
-// .then(handleSuccess)
-
-
-// function onClick (){
-//   var canvas = document.getElementById('snapshot')
-//   var context = canvas.getContext('2d')
-//   var image = new Image()
-//   console.log('BUTTON CLICKITY');
-//   context.drawImage(image, 0, 0, canvas.width, canvas.height)
-// }
-
-
-// ##Input function - works to access camera and take photo - can't save.
-// <video id='video' width=300 height=300></video>
