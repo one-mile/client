2204a3ae29de24a52b58003c4a08c198fc152a8d
harrison-symes <symeshjb@gmail.com>
2016-12-12 10:59:14 +1300

    Generalize renderEntries FN; consolidate

--- components/target.js
+++ components/target.js
@@ -1,45 +1,15 @@
 var yo = require('yo-yo')
 const header = require ('./header').header
 const footer = require ('./header').footer
-
+const renderEntries = require('./renderEntries')
 const url = require('./requestUrl')
 
 function target (state, dispatch) {
   return yo `
   <div class='homediv'>
     <p>hello I am other</p>
-    ${renderEntries(state, dispatch)}
+    ${renderEntries(state, dispatch, state.targetEntries)}
   </div>
-  `
-}
-
-// REPETITION
-
-function renderEntries (state, dispatch) {
-  return yo `
-    <div class='entries'>
-      ${state.targetEntries.map( (entry) => {
-        return renderEntry(entry, state, dispatch)
-      } )}
-    </div>
-  `
-}
-
-function renderEntry(entry, state, dispatch) {
-  return yo`
-  <div class='entry'>
-    ${entryHeader(entry, state, dispatch)}
-    <img src=${entry.image_url}></img>
-  </div>
-  `
-}
-
-function entryHeader(entry, state, dispatch) {
-  var timeDateEntry = entry.entry_created_at // In prep for date/time reformatting
-  return yo`
-    <div class='image-header'>
-        <h2>Added at: ${timeDateEntry} </h2>
-    </div>
   `
 }
 
--- components/user.js
+++ components/user.js
@@ -1,6 +1,7 @@
 var yo = require('yo-yo')
 const header = require ('./header').header
 const footer = require ('./header').footer
+const renderEntries = require('./renderEntries')
 
 const url = require('./requestUrl')
 
@@ -8,38 +9,8 @@ function user (state, dispatch) {
   return yo `
   <div class="homediv">
     <p>hello I am me</p>
-    ${renderEntries(state, dispatch)}
+    ${renderEntries(state, dispatch, state.myEntries)}
   </div>
-  `
-}
-
-// REPETITION
-
-function renderEntries (state, dispatch) {
-  return yo `
-    <div class='entries'>
-      ${state.myEntries.map( (entry) => {
-        return renderEntry(entry, state, dispatch)
-      } )}
-    </div>
-  `
-}
-
-function renderEntry(entry, state, dispatch) {
-  return yo`
-  <div class='entry'>
-    ${entryHeader(entry, state, dispatch)}
-    <img src=${entry.image_url}></img>
-  </div>
-  `
-}
-
-function entryHeader(entry, state, dispatch) {
-  var timeDateEntry = entry.entry_created_at // In prep for date/time reformatting
-  return yo`
-    <div class='image-header'>
-        <h2>Added at: ${timeDateEntry} </h2>
-    </div>
   `
 }
 
