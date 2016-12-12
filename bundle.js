23b3d4a869537490b991b2bcf5e548a73960cac6
kfrn <knfrances@gmail.com>
2016-12-13 11:40:02 +1300

    Updates

--- bundle.js
+++ bundle.js
@@ -11150,7 +11150,7 @@ module.exports = renderEntries;
 },{"./comments":47,"./requestUrl":53,"moment":13,"superagent":34,"yo-yo":43}],53:[function(require,module,exports){
 var heroku = 'http://one-shot-api.herokuapp.com/api/v1/';
 var local = 'http://localhost:3000/api/v1/';
-var url = local;
+var url = heroku;
 
 module.exports = url;
 
