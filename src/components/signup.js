53fac0f9a4cd98481b2b5a613077ee44d70d4cc5
kfrn <knfrances@gmail.com>
2016-12-13 16:23:37 +1300

    Change input type to password; update widget to only display camera

--- src/components/signup.js
+++ src/components/signup.js
@@ -12,8 +12,8 @@ function signup (state, dispatch) {
       <h1>Sign Up</h1>
       <form class='signup'>
         <input id='username' type='text' placeholder='Choose username'/>
-        <input id='password' type='text' placeholder='Choose password'/>
-        <input id='rpt-password' type='text' placeholder='Confirm password'/>
+        <input id='password' type='password' placeholder='Choose password'/>
+        <input id='rpt-password' type='password' placeholder='Confirm password'/>
         <input id='email' type='text' placeholder='Enter email'/>
         ${state.authError ? yo`<h3>${state.authError}</h3>` : ""}
         <button onclick=${handleSignup} class='signupBtn' type='submit'>Create Account</button>
