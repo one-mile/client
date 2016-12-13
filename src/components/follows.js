0f2e3f2736f550389d12eb096bfb11fb5718d59c
harrison-symes <symeshjb@gmail.com>
2016-12-13 19:17:16 +1300

    export requests for home and follow entries

--- src/components/follows.js
+++ src/components/follows.js
@@ -3,47 +3,17 @@ const request = require('superagent')
 const onload = require('on-load')
 const url = require('./requestUrl')
 const renderEntries = require('./renderEntries')
+const followEntries = require('./refreshFunctions/followEntries')
 
 function follows (state, dispatch) {
   return yo `
   <div class="homediv">
       "follows"
       ${state.isLoading ? yo`<p>loading</p>` : renderEntries(state, dispatch, state.followEntries)}
-    ${getFollows(state, dispatch)}
-    <button onclick=${()=>{getFollows(state, dispatch, true)}}>get follows</button>
+    ${followEntries(state, dispatch)}
+    <button onclick=${()=>{followEntries(state, dispatch, true)}}>get follows</button>
   </div>
   `
-}
-
-function getFollows (state, dispatch, bool) {
-  if (state.myFollowing != null) {
-    if (state.followEntries.length === 0 && !state.isLoading || bool) {
-      dispatch({type: "TOGGLE_LOADING"})
-      request
-        .get(url + 'entries/follows/' + state.user.user_id)
-        .end( (error, res) => {
-          console.log("memes", res);
-          if (error) console.log(error);
-          else if (res.body.followed_entries.length == 0) {
-            dispatch({type: 'RECIEVE_FOLLOW_ENTRIES', payload: null})
-          } else {
-            console.log("follow entries");
-            dispatch({type: 'RECIEVE_FOLLOW_ENTRIES', payload: res.body})
-          }
-        })
-    }
-  } else if (!state.isLoading){
-    dispatch({type: "TOGGLE_LOADING"})
-    request
-      .get(url + 'entries/' + state.user.user_id)
-      .end( (error, res) => {
-
-        if (error) console.log(error);
-        else {
-          dispatch({type: 'RECEIVE_ENTRIES', payload: res.body})
-        }
-      })
-  }
 }
 
 module.exports = follows
