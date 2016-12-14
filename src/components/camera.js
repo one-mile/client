313c7d96d081c38529047c272e335386c12216d9
kfrn <knfrances@gmail.com>
2016-12-14 19:48:29 +1300

    Change cloud name & upload preset to my account

--- src/components/camera.js
+++ src/components/camera.js
@@ -6,7 +6,7 @@ const url = require('./requestUrl')
 //
 function accessCamera (state, dispatch) {
   if(state.user.shotsRemaining > 0) {
-    cloudinary.openUploadWidget({ cloud_name: 'toothandpail', upload_preset: 'fasiveib', sources: ['camera'], default_source: 'local', multiple: false, theme: 'minimal', button_class: 'flooki_button' },
+    cloudinary.openUploadWidget({ cloud_name: 'flooki', upload_preset: 'clientuploads', sources: ['camera'], default_source: 'local', multiple: false, theme: 'minimal', button_class: 'flooki_button' },
       function (err, result) {
         if (result) {
           request
@@ -25,6 +25,3 @@ function accessCamera (state, dispatch) {
 }
 
 module.exports = accessCamera
-
-// cloud_name: 'toothandpail', upload_preset: 'fasiveib'
-// cloud_name: 'dr2qeam2p', upload_preset: 'iyjqsx0w'
